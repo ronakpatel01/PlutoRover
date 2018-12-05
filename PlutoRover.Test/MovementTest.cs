48bcc153970dad6cc42a9acd95f45a9495643efb
Ronak Patel <ronak.patel@dolphin-developers.com>
2018-12-05 21:27:25 +0000

    Test methods for forward and backwards movements in place

--- PlutoRover.Test/MovementTest.cs
+++ PlutoRover.Test/MovementTest.cs
@@ -7,12 +7,86 @@ namespace PlutoRover.Test
     public class MovementTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSetup()
         {
-            // Test initial setup is working
-            Movement m = new Movement();
+            RoverPositionModule m = new RoverPositionModule(0, 0, Direction.North);
 
-            Assert.AreEqual(1, 1);
+            Assert.AreEqual(0, m.xPos);
+            Assert.AreEqual(0, m.yPos);
+            Assert.AreEqual(Direction.North, m.direction);
         }
+
+        [TestMethod]
+        public void TestForwardMovementVertical()
+        {
+            RoverPositionModule m = new RoverPositionModule(0, 5, Direction.North);
+
+            m.MoveCommand("F");
+
+            Assert.AreEqual(0, m.xPos);
+            Assert.AreEqual(6, m.yPos);
+            Assert.AreEqual(Direction.North, m.direction);
+        }
+
+        [TestMethod]
+        public void TestBackwardsMovementVertical()
+        {
+            RoverPositionModule m = new RoverPositionModule(0, 5, Direction.North);
+
+            m.MoveCommand("B");
+
+            Assert.AreEqual(0, m.xPos);
+            Assert.AreEqual(4, m.yPos);
+            Assert.AreEqual(Direction.North, m.direction);
+        }
+
+        [TestMethod]
+        public void TestForwardBackwardsMovementVertical()
+        {
+            RoverPositionModule m = new RoverPositionModule(0, 5, Direction.North);
+
+            m.MoveCommand("FFBFFB");
+
+            Assert.AreEqual(0, m.xPos);
+            Assert.AreEqual(7, m.yPos);
+            Assert.AreEqual(Direction.North, m.direction);
+        }
+
+        [TestMethod]
+        public void TestForwardMovementHorizontal()
+        {
+            RoverPositionModule m = new RoverPositionModule(5, 0, Direction.East);
+
+            m.MoveCommand("F");
+
+            Assert.AreEqual(0, m.xPos);
+            Assert.AreEqual(6, m.yPos);
+            Assert.AreEqual(Direction.East, m.direction);
+        }
+
+        [TestMethod]
+        public void TestBackwardsMovementHorizontal()
+        {
+            RoverPositionModule m = new RoverPositionModule(5, 0, Direction.East);
+
+            m.MoveCommand("B");
+
+            Assert.AreEqual(6, m.xPos);
+            Assert.AreEqual(0, m.yPos);
+            Assert.AreEqual(Direction.East, m.direction);
+        }
+
+        [TestMethod]
+        public void TestForwardBackwardsMovementHorizontal()
+        {
+            RoverPositionModule m = new RoverPositionModule(0, 5, Direction.East);
+
+            m.MoveCommand("FFBFFB");
+
+            Assert.AreEqual(0, m.xPos);
+            Assert.AreEqual(7, m.yPos);
+            Assert.AreEqual(Direction.East, m.direction);
+        }
+
     }
 }
---
+++ PlutoRover/RoverPositionModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PlutoRover
+{
+    public class RoverPositionModule
+    {
+        public int xPos { get; private set; }
+        public int yPos { get; private set; }
+        public Direction direction { get; private set; }
+
+        public RoverPositionModule(int x, int y, Direction direction)
+        {
+            xPos = x;
+            yPos = y;
+            this.direction = direction;
+        }
+
+        public void MoveCommand(string movementString)
+        {
+
+        }
+    }
+}
