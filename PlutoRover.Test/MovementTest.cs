4350207586cc9be85fd9333bdbcd37d46926051e
Ronak Patel <ronak.patel@dolphin-developers.com>
2018-12-05 22:46:23 +0000

    Added Tests for wrapping

--- PlutoRover.Test/MovementTest.cs
+++ PlutoRover.Test/MovementTest.cs
@@ -16,6 +16,7 @@ namespace PlutoRover.Test
             Assert.AreEqual(Direction.North, m.direction);
         }
 
+        #region straight movement
         [TestMethod]
         public void TestForwardMovementVertical()
         {
@@ -87,7 +88,9 @@ namespace PlutoRover.Test
             Assert.AreEqual(0, m.yPos);
             Assert.AreEqual(Direction.East, m.direction);
         }
+        #endregion straight movement
 
+        #region Rotational movement
         [TestMethod]
         public void TestRotateClockwise()
         {
@@ -147,5 +150,118 @@ namespace PlutoRover.Test
             Assert.AreEqual(8, m.yPos);
             Assert.AreEqual(Direction.East, m.direction);
         }
+        #endregion Rotational movement
+
+        #region Wrapping        
+        [TestMethod]
+        public void WrapNorthForward()
+        {
+            RoverPositionModule m = new RoverPositionModule(95, 95, Direction.North);
+
+            m.MoveCommand("FFFFFFFFFF");
+
+            Assert.AreEqual(95, m.xPos);
+            Assert.AreEqual(4, m.yPos);
+            Assert.AreEqual(Direction.North, m.direction);
+        }
+
+        [TestMethod]
+        public void WrapNorthBackward()
+        {
+            RoverPositionModule m = new RoverPositionModule(5, 5, Direction.North);
+
+            m.MoveCommand("BBBBBBBBBB");
+
+            Assert.AreEqual(5, m.xPos);
+            Assert.AreEqual(96, m.yPos);
+            Assert.AreEqual(Direction.North, m.direction);
+        }
+
+        [TestMethod]
+        public void WrapEastForward()
+        {
+            RoverPositionModule m = new RoverPositionModule(95, 95, Direction.East);
+
+            m.MoveCommand("FFFFFFFFFF");
+
+            Assert.AreEqual(4, m.xPos);
+            Assert.AreEqual(95, m.yPos);
+            Assert.AreEqual(Direction.East, m.direction);
+        }
+
+        [TestMethod]
+        public void WrapEastBackward()
+        {
+            RoverPositionModule m = new RoverPositionModule(5, 5, Direction.East);
+
+            m.MoveCommand("BBBBBBBBBB");
+
+            Assert.AreEqual(96, m.xPos);
+            Assert.AreEqual(5, m.yPos);
+            Assert.AreEqual(Direction.East, m.direction);
+        }
+
+        [TestMethod]
+        public void WrapSouthForward()
+        {
+            RoverPositionModule m = new RoverPositionModule(5, 5, Direction.South);
+
+            m.MoveCommand("FFFFFFFFFF");
+
+            Assert.AreEqual(5, m.xPos);
+            Assert.AreEqual(96, m.yPos);
+            Assert.AreEqual(Direction.South, m.direction);
+        }
+
+        [TestMethod]
+        public void WrapSouthBackward()
+        {
+            RoverPositionModule m = new RoverPositionModule(95, 95, Direction.South);
+
+            m.MoveCommand("BBBBBBBBBB");
+
+            Assert.AreEqual(95, m.xPos);
+            Assert.AreEqual(4, m.yPos);
+            Assert.AreEqual(Direction.South, m.direction);
+        }
+
+        [TestMethod]
+        public void WrapWestForward()
+        {
+            RoverPositionModule m = new RoverPositionModule(95, 95, Direction.West);
+
+            m.MoveCommand("FFFFFFFFFF");
+
+            Assert.AreEqual(4, m.xPos);
+            Assert.AreEqual(95, m.yPos);
+            Assert.AreEqual(Direction.West, m.direction);
+
+        }
+
+        [TestMethod]
+        public void WrapWestBackward()
+        {
+            RoverPositionModule m = new RoverPositionModule(5, 5, Direction.West);
+
+            m.MoveCommand("BBBBBBBBBB");
+
+            Assert.AreEqual(96, m.xPos);
+            Assert.AreEqual(5, m.yPos);
+            Assert.AreEqual(Direction.West, m.direction);
+        }
+
+        public void WrappingRoundRoute()
+        {
+            RoverPositionModule m = new RoverPositionModule(95, 95, Direction.North);
+
+            m.MoveCommand("FFFFFFFFFFRFFFFFFFFFFRFFFFFFFFFFRFFFFFFFFFFR");
+
+            Assert.AreEqual(95, m.xPos);
+            Assert.AreEqual(95, m.yPos);
+            Assert.AreEqual(Direction.North, m.direction);
+        }
+
+
+        #endregion Wrapping
     }
 }
---
+++ PlutoRover/Model/WorldConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlutoRover.Model
+{
+    public class WorldConstants
+    {
+        public int xMax { get; } = 100;
+        public int yMax { get; } = 100;
+    }
+}
